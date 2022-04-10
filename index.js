2e0552efde61c6407eb521e28a77c9fa4ed786e1
theesari4s <103382623+theesari4s@users.noreply.github.com>
2022-04-10 22:19:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە lera Link Dane`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/sEjDP765Rw`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("id Channel ").send(
+  client.channels.get("962791339127369828 ").send(
 `> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`RaMa is here`) 
+client.user.setGame(`Sarias Is Here`) 
 });
 
 
 
 
-client.login("");// Token Lera Dane
+client.login("OTYyNzg4NDQ2ODgyMTE5Nzcw.YlMonQ.57khLjIdEJlgO2P3oB7GwobyyO4");// Token Lera Dane
